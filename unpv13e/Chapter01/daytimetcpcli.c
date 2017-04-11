e77080c34d0b257ddd273ad8e374a560bbc0d7ec
YuanFanBin <yuanfanbin@gmail.com>
2017-04-11 17:36:51 +0800

    unpv13e: add intro/daytimetcpcli.c

--- unpv13e/Chapter01/daytimetcpcli.c
+++ unpv13e/Chapter01/daytimetcpcli.c
@@ -1,41 +1,53 @@
 /* intro/daytimetcpcli */
-#include "unp.h"
+#include <stdio.h>      /* fputs */
+#include <errno.h>      /* errno */
+#include <netinet/in.h> /* socket, sockaddr_in */
+#include <strings.h>    /* bzero */
+#include <arpa/inet.h>  /* inet_pton */
+#include <sys/types.h>  /* connect, read */
+#include <sys/socket.h> /* connect */
+#include <sys/uio.h>    /* read */
+#include <unistd.h>     /* read */
+#include "error.h"      /* err_* */
+
+#define MAXLINE 4096    /* max text line length */
 
 /* figure-1.5 */
-/* gcc unp.h error.c daytimetcpcli.c */
-int
-main(int argc, char **argv)
+/* $ gcc error.h daytimetcpcli.c */
+int main(int argc, char **argv)
 {
     int                 sockfd, n;
     char                recvline[MAXLINE + 1];
     struct sockaddr_in  servaddr;
 
-    if (argc != 2)
+    if (argc != 2) {
         err_quit("usage: a.out <IPaddress>");
+    }
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         err_sys("socket error");
+    }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     /* /etc/services 中查看端口对应的服务 */
     servaddr.sin_port = htons(13);      /* daytime server */
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
         err_quit("inet_pton error for %s", argv[1]);
+    }
 
-    if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
         err_sys("connect error, errno = %d", errno);
+    }
 
     while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0;        /* null terminate */
-        if (fputs(recvline, stdout) == EOF)
+        if (fputs(recvline, stdout) == EOF) {
             err_sys("fputs error");
+        }
     }
-    if (n < 0)
+    if (n < 0) {
         err_sys("read error");
+    }
     exit(0);
 }
-
-/*
- * connection refused, errno = 111，不过理解大概意思了。
- */
--- unpv13e/Chapter01/error.h
+++ unpv13e/Chapter01/error.h
@@ -1,23 +1,31 @@
-#include "unp.h"
+#ifndef __error_h__
+#define __error_h__
 
-#include <stdarg.h>     /* ANSI C header file */
-#include <syslog.h>     /* for syslog() */
+#include <stdio.h>   /* vsprintf */
+#include <stdarg.h>  /* ANSI C header file */
+#include <syslog.h>  /* for syslog() */
+#include <stdlib.h>  /* exit */
+#include <errno.h>   /* errno */
+#include <string.h>  /* strlen */
 
-int daemon_proc;        /* set nonzero by daemon_init() */
+#define MAXLINE 4096 /* max text line length */
 
-static void err_doit(int, int, const char *, va_list);
+int daemon_proc;     /* set nonzero by daemon_init() */
+
+static void err_doit (int, int, const char *, va_list);
 
 /* 
  * Nonfatal error related to system call
  * Print message and return 
  */
-void
-err_ret(const char *fmt, ...)
+void err_ret (const char *fmt, ...)
 {
-    va_list     ap;
+    va_list ap;
+    
     va_start(ap, fmt);
     err_doit(1, LOG_INFO, fmt, ap);
     va_end(ap);
+
     return;
 }
 
@@ -25,10 +33,9 @@ err_ret(const char *fmt, ...)
  * Fatal error related to system call
  * Print message and terminate
  */
-void
-err_sys(const char *fmt, ...)
+void err_sys (const char *fmt, ...)
 {
-    va_list     ap;
+    va_list ap;
 
     va_start(ap, fmt);
     err_doit(1, LOG_ERR, fmt, ap);
@@ -40,30 +47,29 @@ err_sys(const char *fmt, ...)
  * Fatal error related to system call
  * Print message, dump core, and terminate
  */
-void
-err_dump(const char *fmt, ...)
+void err_dump (const char *fmt, ...)
 {
-    va_list     ap;
+    va_list ap;
 
     va_start(ap, fmt);
     err_doit(1, LOG_ERR, fmt, ap);
     va_end(ap);
-    abort();            /* dump core and terminate */
-    exit(1);            /* shouldn't get here */
+    abort();    /* dump core and terminate */
+    exit(1);    /* shouldn't get here */
 }
 
 /* 
  * Nonfatal error unrelated to system call
  * Print message and return
  */
-void
-err_msg(const char *fmt, ...)
+void err_msg (const char *fmt, ...)
 {
-    va_list     ap;
+    va_list ap;
 
     va_start(ap, fmt);
     err_doit(0, LOG_INFO, fmt, ap);
     va_end(ap);
+
     return;
 }
 
@@ -71,10 +77,9 @@ err_msg(const char *fmt, ...)
  * Fatal error unrelated to system call
  * Print message and terminate
  */
-void
-err_quit(const char *fmt, ...)
+void err_quit (const char *fmt, ...)
 {
-    va_list     ap;
+    va_list ap;
 
     va_start(ap, fmt);
     err_doit(0, LOG_ERR, fmt, ap);
@@ -86,17 +91,16 @@ err_quit(const char *fmt, ...)
  * Print message and return to caller
  * Caller specifies "errnoflag" and "level"
  */
-static void
-err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+static void err_doit (int errnoflag, int level, const char *fmt, va_list ap)
 {
     int     errno_save, n;
     char    buf[MAXLINE + 1];
 
-    errno_save = errno;                 /* value caller might want printed */
+    errno_save = errno; /* value caller might want printed */
 #ifdef HAVE_VSNPRINTF
     vsnprintf(buf, MAXLINE, fmt, ap);   /* safe */
 #else
-    vsprintf(buf, fmt, ap);             /* not safe */
+    vsprintf(buf, fmt, ap); /* not safe */
 #endif
     n = strlen(buf);
     if (errnoflag) {
@@ -107,9 +111,10 @@ err_doit(int errnoflag, int level, const char *fmt, va_list ap)
     if (daemon_proc) {
         syslog(level, buf);
     } else {
-        fflush(stdout);                 /* in case stdout and stderr are the same */
+        fflush(stdout); /* in case stdout and stderr are the same */
         fputs(buf, stderr);
         fflush(stderr);
     }
     return;
 }
+#endif
