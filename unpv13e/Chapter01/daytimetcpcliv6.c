1cb1835f63df6cf609f224579dda3e42afd328c2
YuanFanBin <yuanfanbin@gmail.com>
2017-04-12 10:58:28 +0800

    unpv13e: add intro/daytimetcpcliv6.c intro/daytimetcpsrc.c

--- unpv13e/Chapter01/daytimetcpcliv6.c
+++ unpv13e/Chapter01/daytimetcpcliv6.c
@@ -1,38 +1,52 @@
 /* intro/daytimetcpcli */
-#include "unp.h"
+#include <stdio.h>      /* fputs */
+#include <errno.h>      /* errno */
+#include <netinet/in.h> /* socket, sockaddr_in */
+#include <strings.h>    /* bzero */
+#include <arpa/inet.h>  /* inet_pton */
+#include <sys/types.h>  /* connect, read */
+#include <sys/socket.h> /* connect */
+#include <sys/uio.h>    /* read */
+#include <unistd.h>     /* read */
+#include "error.h"      /* err_* */
 
 /* figure-1.6 */
-/* gcc unp.h error.c daytimetcpcliv6.c */
-int
-main(int argc, char **argv)
+/* $ gcc error.h daytimetcpcliv6.c */
+int main(int argc, char **argv)
 {
     int                 sockfd, n;
     char                recvline[MAXLINE + 1];
     struct sockaddr_in6 servaddr;
 
-    if (argc != 2)
+    if (argc != 2) {
         err_quit("usage: a.out <IPaddress>");
+    }
 
-    if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+    if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
         err_sys("socket error");
+    }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin6_family = AF_INET6;
     /* /etc/services 中查看端口对应的服务 */
     servaddr.sin6_port = htons(13);      /* daytime server */
-    if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0)
+    if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0) {
         err_quit("inet_pton error for %s", argv[1]);
+    }
 
-    if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
         err_sys("connect error, errno = %d", errno);
+    }
 
     while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0;        /* null terminate */
-        if (fputs(recvline, stdout) == EOF)
+        if (fputs(recvline, stdout) == EOF) {
             err_sys("fputs error");
+        }
     }
-    if (n < 0)
+    if (n < 0) {
         err_sys("read error");
+    }
     exit(0);
 }
 
--- unpv13e/Chapter01/daytimetcpsrc.c
+++ unpv13e/Chapter01/daytimetcpsrc.c
@@ -1,10 +1,19 @@
-#include "unp.h"
-#include <time.h>
+#include <stdio.h>      /* snprintf */
+#include <string.h>     /* strlen */
+#include <arpa/inet.h>  /* htonl */
+#include <netinet/in.h> /* sockaddr_in */
+#include <strings.h>    /* bzero */
+#include <sys/socket.h> /* socket, bind, listen, accept */
+#include <time.h>       /* time, ctime */
+#include <unistd.h>     /* write, close */
+#include "error.h"      /* err_* */
+
+#define MAXLINE 4096    /* max text line length */
+#define LISTENQ 1024    /* 2nd argument to listen() */
 
 /* figure-1.9 */
-/* gcc unp.h error.c daytimetcpsrc.c */
-int
-main(int argc, char **argv)
+/* $ gcc error.h daytimetcpsrc.c */
+int main(int argc, char **argv)
 {
     int                 err, n;
     int                 listenfd, connfd;
@@ -12,8 +21,9 @@ main(int argc, char **argv)
     char                buff[MAXLINE];
     time_t              ticks;
 
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         err_sys("socket error");
+    }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -21,30 +31,34 @@ main(int argc, char **argv)
     /* /etc/services */
     servaddr.sin_port = htons(13);      /* daytime server */
 
-    if ((err = bind(listenfd, (struct sockaddr *) &servaddr, 
-                    sizeof(servaddr))) < 0)
+    if ((err = bind(listenfd, (struct sockaddr *) &servaddr,
+                    sizeof(servaddr))) < 0) {
         err_sys("bind error");
-    if ((err = listen(listenfd, LISTENQ)) < 0)  //  # 0
-    //if ((err = listen(listenfd, 2)) < 0)          # 1
+    }
+    //if ((err = listen(listenfd, 2)) < 0) {        # 1
+    if ((err = listen(listenfd, LISTENQ)) < 0) {  //  # 0
         err_sys("listen error");
+    }
 
     for ( ; ; ) {
 again:
         if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) < 0) {
 #ifdef EPROTO
-            if (errno == EPROTO || errno == ECONNABORTED)
+            if (errno == EPROTO || errno == ECONNABORTED) {
 #else
-            if (errno == ECONNABORTED)
+            if (errno == ECONNABORTED) {
 #endif
                 goto again;
-            else
+            } else {
                 err_sys("accept error");
+            }
         }
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
         /* 忽略2个strlen问题 */
-        if ((n = write(connfd, buff, strlen(buff))) != strlen(buff))
+        if ((n = write(connfd, buff, strlen(buff))) != strlen(buff)) {
             err_sys("write error");
+        }
         //sleep(3);                                 # 2
         close(connfd);
     }
