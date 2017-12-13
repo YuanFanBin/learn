722b36ae92f9e29d5b3efe181b952d35b91834c8
yuanfanbin <yuanfanbin@github.com>
2017-12-13 17:00:08 +0800

    update Chapter03

--- unpv13e/Chapter03/readline.c
+++ unpv13e/Chapter03/readline.c
@@ -2,34 +2,6 @@
 #include <unistd.h>     /* read */
 #include <sys/types.h>  /* ssize_t */
 
-/* PAINFULLY SLOW VERSION -- example only */
-ssize_t readline(int fd, void *vptr, size_t maxlen)
-{
-    ssize_t n, rc;
-    char    c, *ptr;
-
-    ptr = vptr;
-    for (n = 1; n < maxlen; n++) {
-again:
-        if ((rc = read(fd, &c, 1)) == 1) {  /* 每读取一个字节，进行一次系统调用，速度极慢 */
-            *ptr++ = c;
-            if (c == '\n') {
-                break;      /* newline is stored, like fgets() */
-            }
-        } else if (rc == 0) {
-            *ptr = 0;
-            return(n - 1);  /* EOF, n - 1 bytes were read */
-        } else {
-            if (errno == EINTR) {
-                goto again;
-            }
-            return(-1);     /* error, errno set by read */
-        }
-    }
-    *ptr = 0;   /* null terminate like fgets() */
-    return(n);
-}
-
 #define MAXLINE 4096    /* max text line length */
 
 static int  read_cnt;
@@ -55,6 +27,31 @@ again:
     *ptr = *read_ptr++;
     return(1);
 }
+
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+    ssize_t n, rc;
+    char    c, *ptr;
+
+    ptr = vptr;
+    for (n = 1; n <= maxlen; n++) {
+        if ((rc = my_read(fd, &c)) == 1) {
+            *ptr++ = c;
+            if (c == '\n') {
+                break;      /* newline is stored, like fgets() */
+            }
+        } else if (rc == 0) {
+            *ptr = 0;
+            return(n - 1);  /* EOF, n - 1 bytes were read */
+        } else {
+            return(-1);     /* error, error set by read() */
+        }
+    }
+
+    *ptr = 0;   /* null terminate like fgets() */
+    return(n);
+}
+
 
 ssize_t readlinebuf(void **vptrptr)
 {
---
+++ unpv13e/Chapter03/readline1.c
@@ -0,0 +1,32 @@
+#include <errno.h>      /* EINTR */
+#include <unistd.h>     /* read */
+#include <sys/types.h>  /* ssize_t */
+
+/* PAINFULLY SLOW VERSION -- example only */
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+    ssize_t n, rc;
+    char    c, *ptr;
+
+    ptr = vptr;
+    for (n = 1; n < maxlen; n++) {
+again:
+        if ((rc = read(fd, &c, 1)) == 1) {  /* 每读取一个字节，进行一次系统调用，速度极慢 */
+            *ptr++ = c;
+            if (c == '\n') {
+                break;      /* newline is stored, like fgets() */
+            }
+        } else if (rc == 0) {
+            *ptr = 0;
+            return(n - 1);  /* EOF, n - 1 bytes were read */
+        } else {
+            if (errno == EINTR) {
+                goto again;
+            }
+            return(-1);     /* error, errno set by read */
+        }
+    }
+    *ptr = 0;   /* null terminate like fgets() */
+    return(n);
+}
+
